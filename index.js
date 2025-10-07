ff4488f126e28cd3cb80294f3a8215a143f2e1ca
Humaira Muzammil <humaihr469@gmail.com>
2025-10-07 08:42:47 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,71 +1,3 @@
-// import express from "express";
-// import bodyParser from "body-parser";
-// import axios from "axios";
-// import dotenv from "dotenv";
-// import lodash from "lodash";
-// import bcrypt from "bcrypt";
-// import session from "express-session";
-// import passport from "passport";
-// import cors from "cors";
-
-// dotenv.config();
-
-// const app = express();
-// const port = process.env.PORT || 9000;
-// const rapidApiKey = process.env.RAPIDAPIKEY
-// const rapidApiHost = process.env.RAPIDAPIHOST
-
-// app.use(bodyParser.json());
-// app.use(cors());
-
-// const options = {
-//   method: 'GET',
-//   url: `https://${rapidApiHost}/trims`,
-//   params: {
-//     year: '2019',
-//     make: 'toyota',
-//     model: 'Camry'
-//   },
-//   headers: {
-//     'x-rapidapi-key': rapidApiKey,
-//     'x-rapidapi-host': rapidApiHost
-//   }
-// };
-
-// // async function fetchData() {
-// // 	try {
-// // 		const response = await axios.request(options);
-// // 		console.log(response.data);
-// // 	} catch (error) {
-// // 		console.error(error);
-// // 	}
-// // }
-
-// // fetchData();
-
-// app.get("/", (req, res) => {
-//     res.send("Hello World!");
-// });
-
-// app.get("/carData", async (req, res) => {
-//     try {
-//       const response = await axios.request(options);
-//       console.log(JSON.stringify(response.data.data.Trims, null, 2));
-//       console.log('Type of Data: ', typeof response.data);
-//       console.log(Object.keys(response.data));
-
-
-//       res.json(response.data.data.Trims);
-//     } catch (error) {
-//         console.error(error);
-//     }
-// });
-
-// app.listen(port, () => {
-//     console.log(`Server listening on port ${port}`);
-// });
-
-
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
@@ -86,9 +18,6 @@ const rapidApiHost = process.env.RAPIDAPIHOST
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
 
 app.get("/carData", async (req, res) => {
     try {
