17ee623efc663fed8cbbb4e6e2c5707b82db899a
Humaira Muzammil <humaihr469@gmail.com>
2025-10-09 08:42:08 +0530

    add dbConfig.js and passportConfig.js

---
+++ dbConfig.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+import pkg from 'pg';
+
+const { Pool } = pkg;
+
+dotenv.config();
+
+const isProduction = process.env.NODE_ENV === "production";
+const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
+const pool = new Pool({
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: isProduction
+    ? { rejectUnauthorized: false } // Use SSL only in production
+    : false, // Disable SSL for local development
+});
+
+export { pool };
